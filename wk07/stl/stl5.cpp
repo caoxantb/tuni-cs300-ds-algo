e5157acd7119c8eee021775fedc5e808b58fc23c
Xuan-An Cao <an.cao@tuni.fi>
2023-10-21 14:47:47 +0300

    solve: exercise: 7.2.5

--- wk07/stl/stl5.cpp
+++ wk07/stl/stl5.cpp
@@ -15,8 +15,18 @@ using namespace std;
  * @param v vector to be sorted
  * @return int EXIT_SUCCESS if everything went OK, EXIT_FAILURE otherwise
  */
-int sortMod3(std::vector<int>& v)
-{
-    return EXIT_FAILURE;
+int sortMod3(std::vector<int>& v) {
+    auto itRemainZero = std::partition(v.begin(), v.end(), [](int val) {
+      return val % 3 == 0;
+    });
+    auto itRemainOne = std::partition(itRemainZero, v.end(), [](int val) {
+      return val % 3 == 1;
+    });
+
+    std::sort(v.begin(), itRemainZero);
+    std::sort(itRemainZero, itRemainOne);
+    std::sort(itRemainOne, v.end());
+
+    return EXIT_SUCCESS;
 }
 
