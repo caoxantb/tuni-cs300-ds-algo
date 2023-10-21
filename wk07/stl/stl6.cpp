336701b5eeecf1ed6d2be33d3bdbd1a438230ac8
Xuan-An Cao <an.cao@tuni.fi>
2023-10-22 00:36:12 +0300

    fix: key type of each pair to std::string

--- wk07/stl/stl6.cpp
+++ wk07/stl/stl6.cpp
@@ -16,11 +16,11 @@ using namespace std;
  * @return int the real found value
  */
 int findAtLeastGiven(std::map<std::string, int>& m, int given) {
-  auto it = std::find_if(m.begin(), m.end(), [given](const std::pair<int, int>& pair) {
+  auto it = std::find_if(m.begin(), m.end(), [given](const std::pair<std::string, int>& pair) {
     return pair.second >= given;
   });
 
   if (it != m.end()) return (*it).second;
-  else return NOT_FOUND;
+  return NOT_FOUND;
 }
 
