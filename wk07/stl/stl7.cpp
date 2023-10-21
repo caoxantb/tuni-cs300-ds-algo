d88f3848c9400c94d827d6cc7b4863ad044ee42a
Xuan-An Cao <an.cao@tuni.fi>
2023-10-22 00:43:00 +0300

    solve exercise 7.2.7

--- wk07/stl/stl7.cpp
+++ wk07/stl/stl7.cpp
@@ -14,7 +14,11 @@ using namespace std;
  * @param v unsorted vector
  * @return int calculated median of parameter vector
  */
-int findMedian(std::vector<int>& v)
-{
+int findMedian(std::vector<int>& v) {
+  if (v.empty()) return NOT_FOUND;
+
+  std::sort(v.begin(), v.end());
+  int size = v.size();
+  return size % 2 == 0 ? (v[size/2 - 1] + v[size/2])/2 : v[size/2];
 }
 
