b8083264a620acac4fce62ab1364870f90fe70bb
Xuan-An Cao <an.cao@tuni.fi>
2023-10-21 13:00:45 +0300

    solve: exercise 7.2.3

--- wk07/stl/stl1.cpp
+++ wk07/stl/stl1.cpp
@@ -6,16 +6,13 @@
 
 using namespace std;
 
-
 /**
  * @brief sort a given vector to an ascending order
  *
  * @param v vector to be sorted
  * @return int EXIT_SUCCESS if everything went OK, EXIT_FAILURE otherwise
  */
-int sortAsc(std::vector<int>& v)
-{
-    std::sort(v.begin(), v.end());
-    return EXIT_SUCCESS;
+int sortAsc(std::vector<int> &v) {
+  std::sort(v.begin(), v.end());
+  return EXIT_SUCCESS;
 }
-
