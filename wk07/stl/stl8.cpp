c6040ac6313aebcfabd42e4fbce41a15f21bafd7
Xuan-An Cao <an.cao@tuni.fi>
2023-10-22 00:48:00 +0300

    solve exercise 7.2.8

--- wk07/stl/stl8.cpp
+++ wk07/stl/stl8.cpp
@@ -6,15 +6,15 @@
 
 using namespace std;
 
-
 /**
  * @brief Remove from vector v all elements with value less than the limit
  * @param v a vector whose values are to be erased if less than limit
  * @param limit a threshold whose lower values are removed
  * @return int EXIT_SUCCESS if everything went OK, EXIT_FAILURE otherwise
  */
-int removeLessThan(std::vector<int>& v, int limit)
-{
-    return EXIT_FAILURE;
+int removeLessThan(std::vector<int> &v, int limit) {
+  v.erase(std::remove_if(v.begin(), v.end(), [limit](int value) {
+    return value < limit;
+  }), v.end());
+  return EXIT_SUCCESS;
 }
-
