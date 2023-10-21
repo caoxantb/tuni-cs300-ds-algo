830b355f445b63170804d7732e69ed2d14549c09
Xuan-An Cao <an.cao@tuni.fi>
2023-10-21 12:46:45 +0300

    solve: exercise 7.2.2

--- wk07/stl/stl2.cpp
+++ wk07/stl/stl2.cpp
@@ -6,15 +6,14 @@
 
 using namespace std;
 
-
 /**
  * @brief sort a given vector to an descending order
  *
  * @param v a vector to be sorted
  * @return int EXIT_SUCCESS if everything went OK, EXIT_FAILURE otherwise
  */
-int sortDesc(std::vector<int>& v)
+int sortDesc(std::vector<int> &v)
 {
-    return EXIT_FAILURE;
+  std::sort(v.begin(), v.end(), std::greater<int>());
+  return EXIT_SUCCESS;
 }
-
