592d5d5d452af8af6cbc4c59943af2ba1744cb12
Xuan-An Cao <an.cao@tuni.fi>
2023-10-21 14:23:31 +0300

    solve: exercise 7.2.4

--- wk07/stl/stl4.cpp
+++ wk07/stl/stl4.cpp
@@ -6,7 +6,6 @@
 
 using namespace std;
 
-
 /**
  * @brief Return an iterator which points to the last even integer of the vector
  *        If the value is not found, return v.rend()
@@ -14,7 +13,10 @@ using namespace std;
  * @param v the vector to be scanned through
  * @return std::vector<int>::reverse_iterator
  */
-std::vector<int>::reverse_iterator findLastEven(std::vector<int>& v)
+std::vector<int>::reverse_iterator findLastEven(std::vector<int> &v)
 {
+  auto it = std::find_if(v.rbegin(), v.rend(), [](int val) { 
+    return val % 2 == 0; 
+  });
+  return it;
 }
-
