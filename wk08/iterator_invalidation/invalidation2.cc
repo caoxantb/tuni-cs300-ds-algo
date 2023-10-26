3748a74b4ed7b703cbc9803e4d5c17a55f472a2e
Xuan-An Cao <an.cao@tuni.fi>
2023-10-26 18:55:47 +0300

    fix: logic issue exercise 8.2.8

--- wk08/iterator_invalidation/invalidation2.cc
+++ wk08/iterator_invalidation/invalidation2.cc
@@ -4,16 +4,18 @@
 
 using namespace std;
 
-
 /**
  * @brief Erases every second item from the vector. Example: {1, 2, 3, 4} -> {1, 3}
  *
  * @param vec vector where every second item is erased.
  */
-void eraseEverySecond(std::vector<int>& vec) {
-    auto beg = vec.begin();
-    for (auto i = 1; i < vec.size(); i += 2) {
-      vec.erase(beg + i);
+void eraseEverySecond(std::vector<int> &vec) {
+  auto it = vec.begin() + 1;
+  auto end = vec.end();
+  while (it != end) {
+    it = vec.erase(it);
+    if (it != end) {
+      it++;
     }
+  }
 }
-
