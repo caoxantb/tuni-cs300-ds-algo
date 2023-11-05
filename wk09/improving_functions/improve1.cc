12f3735974d5c9381f802649016112a5f8c9c8c4
Xuan-An Cao <an.cao@tuni.fi>
2023-11-05 02:57:39 +0200

    solve: exercise 9.2. - ascending vector

--- wk09/improving_functions/improve1.cc
+++ wk09/improving_functions/improve1.cc
@@ -2,17 +2,16 @@
 #include <vector>
 #include <algorithm>
 
-
 /**
  * @brief creates a vector of integers with ascending numbers from 0 - n-1
  *
  * @param n the size of the vector to be created
  * @return std::vector<int>
  */
-std::vector<int> ascendingVector(int n){
-    std::vector<int> v;
-    for ( int i = 0; i < n ; i++){
-        v.insert(v.end(), i);
-    }
-    return v;
+std::vector<int> ascendingVector(int n) {
+  std::vector<int> v(n);
+  for (int i = 0; i < n; i++) {
+    v[i] = i;
+  }
+  return v;
 }
